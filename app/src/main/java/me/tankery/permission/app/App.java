95e4b0ee82a8c293f433834632a51ba828088af8
Tankery Chen <xjchen@mobvoi.com>
2018-04-19 15:04:03 +0800

    Add LeakCanary for demo App.

--- app/src/main/java/me/tankery/permission/app/App.java
+++ app/src/main/java/me/tankery/permission/app/App.java
@@ -2,6 +2,8 @@ package me.tankery.permission.app;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
 
@@ -13,6 +15,13 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+
+        LeakCanary.install(this);
 
         Timber.uprootAll();
         Timber.plant(new DebugTree());
